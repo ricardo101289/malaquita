e8d5590a40f93582ec3430552cd35dca225b7727
rick <ricardo.galarza@bayteq.com>
2022-05-28 01:44:57 -0500

    firx formulario

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms'
+import  '../../../assets/smtp/smtp.js';
+declare let Email: any;
 
 @Component({
   selector: 'app-formulario',
@@ -12,12 +14,26 @@ export class FormularioComponent implements OnInit {
 
   ngOnInit(): void {
     this.contactForm = this.formBuilder.group({
-      firstname: ['']
+      name: [''],
+      email: [''],
+      telefono: [''],
+      mensaje: [''],
     });
   }
 
   onSubmit() {
     console.log(this.contactForm.value);
+    Email.send({
+      Host : "smtp.yourisp.com",
+      Username : "username",
+      Password : "password",
+      To : 'them@website.com',
+      From : "you@isp.com",
+      Subject : "This is the subject",
+      Body : "And this is the body"
+  }).then(
+    message => alert(message)
+  );
   }
 
 }
